7f18a0ab08c53329696dd49b18fcc7ef28ca57a0
moodang <yssmi@naver.com>
2023-11-29 11:56:40 +0900

    삼항조건 연산자 사용

--- 03/js/script.js
+++ 03/js/script.js
@@ -19,16 +19,6 @@ $(function () {
 
   // 마우스 휠을 조작했을 때 : wheel
   $window.on('wheel', function (e) {
-    // 마우스 휠을 조작했을 때의 이벤트 정보를 보자
-    // console.log(e);
-    // console.log(e.originalEvent.wheelDelta);
-
-    if (e.originalEvent.wheelDelta > 0) {
-      // 휠을 올린 상태 -->
-      $header.removeClass('hide');
-    } else {
-      // 휠을 내린 상태 --> header를 숨기자 --> hide 클래스 부여
-      $header.addClass('hide');
-    }
+    e.originalEvent.wheelDelta > 0 ? $header.addClass('hide') : $header.removeClass('hide');
   });
 });
